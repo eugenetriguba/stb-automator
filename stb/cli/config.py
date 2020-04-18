5cf38278c2c7b4889bff25f60fe45d36e4e7dd97
Eugene Triguba <eugenetriguba@gmail.com>
2020-04-18 05:06:38 +0000

    Add config cli command

--- stb/cli/config.py
+++ stb/cli/config.py
@@ -1,73 +1,107 @@
 import click
 from config_file import ConfigFile, ParsingError
 
-from stb.utils import exit_with_error_output, get_config_file_path
+from stb.utils import exit_with_error_output, get_config_file_path, module_path
 
 
-@click.group(short_help="Print configuration file")
-@click.argument("key", required=False)
-@click.argument("value", required=False)
-@click.option(
-    "-d",
-    "--delete",
-    help="Deletes an entire section or a single key. "
-    "If there is no dot (.), it will assume you want to delete the "
-    "section. Otherwise if you only want to delete a key, use "
-    "'section.key'.",
-)
-@click.option(
-    "-r",
-    "--reset",
-    is_flag=True,
-    help="Restores the configuration file to its original state.",
-)
+@click.group(short_help="Manipulate or see your configuration file")
 @click.option(
     "-p", "--path", "custom_path", help="Absolute path to the configuration file"
 )
-def config(key, value, delete, reset, info, custom_path):
-    """
-    Modify or output the configuration file.
-    When retrieving or setting a configuration value, it should be specified in a
-    section.key format. e.g. 'video.source_pipeline' or 'lirc.remote'.
-    \b
-    KEY: The 'section.key' in the configuration file.
-    VALUE: The value of a key in the configuration file.
-    """
+@click.pass_context
+def config(ctx, custom_path):
     try:
         if custom_path:
-            config_file = ConfigFile(custom_path)
+            ctx.obj = ConfigFile(custom_path)
         else:
-            config_file = ConfigFile(get_config_file_path())
-
-        if reset:
-            config_file.restore_original()
-            print("Configuration file has been restored to its original state.")
-            exit(0)
-
-        if delete:
-            config_file.delete(delete)
-            print(f"{delete} was successfully deleted.")
-            exit(0)
-
-        if key and not value:
-            print(config_file.get(key))
-            exit(0)
-
-        if value:
-            config_file.set(key, value)
-            exit(0)
-
-        print(config_file.stringify())
-
+            ctx.obj = ConfigFile(get_config_file_path())
     except ParsingError as error:
         exit_with_error_output(error)
 
 
+@config.command(short_help="Print out a section or key")
+@click.argument("key")
+@click.pass_context
+def get(ctx, key):
+    """
+    Output a section or key of the configuration file.
+    \b
+    KEY: The 'section.key' in the configuration file.
+    """
+    try:
+        print(ctx.obj.get(key))
+    except ParsingError as error:
+        print(error)
+
+
+@config.command(short_help="Set a key to a value")
+@click.argument("key")
+@click.argument("value")
+@click.pass_context
+def set(ctx, key, value):
+    """
+    Modify a key in the configuration file.
+    \b
+    KEY: The 'section.key' in the configuration file.
+    VALUE: The value to set a key in the configuration file.
+    """
+    internal_config_path = module_path("config/config.original.ini")
+
+    if ctx.obj.path == internal_config_path:
+        print("You're using the internal configuration path, which cannot be edited.")
+        print("Run `stb config setup` instead to setup your user configuration file.")
+    else:
+        ctx.obj.set(key, value)
+        click.secho("Successfully set {key} to {value}", fg="green")
+
+
+@config.command(short_help="Print the configuration file")
+@click.pass_context
+def output(ctx):
+    """Print the configuration file."""
+    print(ctx.obj.stringify())
+
+
+@config.command(short_help="Deletes an entire section or a single key")
+@click.pass_context
+def delete(ctx, key):
+    """
+    Deletes an entire section or a single key.
+
+    If there is no dot (.), it will assume you want to delete the section.
+    Otherwise if you only want to delete a key, use a 'section.key' syntax.
+    """
+    try:
+        ctx.obj.delete(key)
+        click.secho(f"{key} was successfully deleted.", fg="green")
+    except ValueError as error:
+        exit_with_error_output(error)
+
+
+@config.command(short_help="Restores the configuration file to its original state")
+@click.pass_context
+def reset(ctx):
+    """Restores the configuration file to its original state."""
+    internal_config_path = module_path("config/config.original.ini")
+
+    if ctx.obj.path == internal_config_path:
+        click.secho(
+            "You're using the internal configuration file, can't reset.", fg="red"
+        )
+    else:
+        ctx.obj.restore_original(original_file_path=internal_config_path)
+        click.secho(
+            "Configuration file has been restored to its original state.", fg="green"
+        )
+
+
 @config.command(short_help="Lookup what configuration path is currently being used")
-def info():
-    print(f"Current configuration file path: {get_config_file_path()}")
+@click.pass_context
+def info(ctx):
+    """Lookup what configuration path is currently being used."""
+    print(f"Current configuration file path: {ctx.obj.path}")
 
 
-@config.command(short_help="Setup the user's configuration folder at ~/.hc")
+@config.command(short_help="Setup the user's configuration folder at ~/.stb")
 def setup():
     pass
