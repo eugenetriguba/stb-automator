63321c35c2db183d738ee59819f27dbd0e2b22e1
Eugene Triguba <eugenetriguba@gmail.com>
2020-04-25 23:28:44 +0000

    Refactor tests to only use pytest (no unittest)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,22 @@
+import socket
+from unittest import mock
+
 import pytest
 from click.testing import CliRunner
+
+from stb.lirc import Lirc
 
 
 @pytest.fixture
 def runner():
     return CliRunner()
+
+
+@pytest.fixture
+def mock_socket():
+    return mock.Mock(spec=socket.socket)
+
+
+@pytest.fixture
+def mock_lirc(mock_socket):
+    return Lirc(socket=mock_socket)
--- tests/test_lirc.py
+++ tests/test_lirc.py
@@ -1,19 +1,51 @@
-from unittest import main
+import pytest
 
-from stb.lirc import Lirc
-from tests.utils.sockets import LircMockSocketTest
-
-
-class TestLirc(LircMockSocketTest):
-
-    def test_default_lirc_init(self):
-        self.mock_socket.connect.assert_called_with(self.lirc.DEFAULT_SOCKET_PATH)
-
-    def test_custom_path_lirc_init(self):
-        TEST_SOCKET_PATH = "test_path"
-        Lirc(socket=self.mock_socket, socket_path=TEST_SOCKET_PATH)
-        self.mock_socket.connect.assert_called_with(TEST_SOCKET_PATH)
+from stb.lirc import Lirc, LircResponse
 
 
-if __name__ == '__main__':
-    main()
+def test_default_lirc_init(mock_lirc):
+    mock_lirc.socket.connect.assert_called_with(mock_lirc.DEFAULT_SOCKET_PATH)
+
+
+def test_custom_path_lirc_init(mock_socket):
+    TEST_SOCKET_PATH = "test_path"
+    Lirc(socket=mock_socket, socket_path=TEST_SOCKET_PATH)
+    mock_socket.connect.assert_called_with(TEST_SOCKET_PATH)
+
+
+@pytest.mark.parametrize(
+    "reply_packet, success, data, repeat_count",
+    [
+        (
+            b"BEGIN\nSEND_ONCE remote key\nSUCCESS\nEND\n",
+            True,
+            [],
+            1
+        )
+
+    ]
+)
+def test_send_once(mock_lirc, reply_packet, success, data, repeat_count):
+    REMOTE = "remote"
+    KEY = "key"
+    COMMAND = f"SEND_ONCE {REMOTE} {KEY}"
+
+    mock_lirc.socket.recv.return_value = reply_packet
+    response = mock_lirc.send_once(KEY, REMOTE)
+    mock_lirc.socket.sendall.assert_called_with(
+        (COMMAND + "\n").encode(mock_lirc.ENCODING))
+
+    if repeat_count > 1:
+        assert type(response) == list
+        assert len(response) == repeat_count
+
+        for r in response:
+            assert type(r) == LircResponse
+            assert r.command == COMMAND
+            assert r.success == success
+            assert r.data == data
+    else:
+        assert type(response) == LircResponse
+        assert response.command == COMMAND
+        assert response.success == success
+        assert response.data == data
