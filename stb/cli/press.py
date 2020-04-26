b93bfe2388c561bd171dbd49444f57b041c02f5a
Eugene Triguba <eugenetriguba@gmail.com>
2020-04-26 02:19:22 +0000

    Fix short_help on an option

--- stb/cli/press.py
+++ stb/cli/press.py
@@ -7,7 +7,7 @@ from stb.utils import get_config_file_path
 
 @click.command(short_help="Send IR signals")
 @click.argument("key", required=True)
-@click.option("-r", "--remote", default=None, short_help="The remote to press keys on.")
+@click.option("-r", "--remote", default=None, help="The remote to press keys on.")
 def press(key, remote):
     """
     Simulate a button press by sending an IR signal
