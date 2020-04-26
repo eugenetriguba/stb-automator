e31139b05b4b096eed2db1a3830869a8e1f95f7b
Eugene Triguba <eugenetriguba@gmail.com>
2020-04-26 02:12:40 +0000

    Remote is now an option flag rather than an argument

--- stb/cli/press.py
+++ stb/cli/press.py
@@ -1,13 +1,22 @@
 import click
+from config_file import ConfigFile
 
 from stb.remote import Remote
+from stb.utils import get_config_file_path
 
 
 @click.command(short_help="Send IR signals")
-@click.argument("remote", required=True)
 @click.argument("key", required=True)
+@click.option("-r", "--remote", default=None, short_help="The remote to press keys on.")
 def press(key, remote):
-    """Simulate a button press by sending an IR signal"""
+    """
+    Simulate a button press by sending an IR signal
+
+    """
+    if not remote:
+        config = ConfigFile(get_config_file_path())
+        remote = config.get("lirc.remote")
+
     result = Remote(remote).press(key)
     if result.success:
         click.secho(f"Emitted {key} successfully", fg="green")
