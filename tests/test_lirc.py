0228481a9ed203f0cc21625dc50a1659856ef22f
Eugene Triguba <eugenetriguba@gmail.com>
2020-04-28 16:05:30 +0000

    tests: Add lirc socket timeout & parse reply packet tests

--- tests/test_lirc.py
+++ tests/test_lirc.py
@@ -1,10 +1,13 @@
+import os
+
 import pytest
 
-from stb.lirc import Lirc, LircResponse
+from stb.lirc import Lirc, LircResponse, LircSocketTimeoutError
 
 
 def test_default_lirc_init(mock_lirc):
     mock_lirc.socket.connect.assert_called_with(mock_lirc.DEFAULT_SOCKET_PATH)
+    mock_lirc.socket.settimeout.assert_called_with(mock_lirc.SOCKET_TIMEOUT)
 
 
 def test_custom_path_lirc_init(mock_socket):
@@ -19,7 +22,63 @@ def test_custom_path_lirc_init(mock_socket):
 def test_read_reply_packet(mock_lirc, reply_packet):
     mock_lirc.socket.recv.return_value = reply_packet
     retrieved_packet = mock_lirc._Lirc__read_reply_packet()
+    mock_lirc.socket.recv.assert_called_with(256)
     assert reply_packet == retrieved_packet.encode(mock_lirc.ENCODING)
+
+
+@pytest.mark.skipif(
+    os.environ.get("STB_RUN_LONG_TESTS") is None, reason="Test takes too long"
+)
+@pytest.mark.parametrize("reply_packet", [b"INVALID PACKET"])
+def test_read_reply_packet_socket_timeout(mock_lirc, reply_packet):
+    with pytest.raises(LircSocketTimeoutError):
+        mock_lirc.socket.recv.return_value = reply_packet
+        mock_lirc._Lirc__read_reply_packet()
+
+
+@pytest.mark.parametrize(
+    "reply_packet, command, success, data, exception",
+    [
+        (
+            "BEGIN\nSEND_ONCE remote key\nSUCCESS\nEND\n",
+            "SEND_ONCE remote key",
+            True,
+            [],
+            None,
+        ),
+        ("INVALID PACKET", "", False, [], None),
+        (
+            "BEGIN\nSEND_ONCE remote key\nERROR\nINVALID\n",
+            "SEND_ONCE remote key",
+            False,
+            [],
+            ValueError,
+        ),
+        (
+            "BEGIN\nSEND_ONCE remote key\nERROR\nDATA\n1\nunknown remote: remote\n",
+            "SEND_ONCE remote key",
+            False,
+            ["unknown remote: remote"],
+            None,
+        ),
+        (
+            "BEGIN\nSEND_ONCE remote key\nSUCCESS\nDATA\n3\nraa\nblah\nbhal\n",
+            "SEND_ONCE remote key",
+            True,
+            ["raa", "blah", "bhal"],
+            None,
+        ),
+    ],
+)
+def test_parse_reply_packet(mock_lirc, reply_packet, command, success, data, exception):
+    if exception:
+        with pytest.raises(exception):
+            mock_lirc._Lirc__parse_reply_packet(reply_packet)
+    else:
+        response = mock_lirc._Lirc__parse_reply_packet(reply_packet)
+        assert response.success == success
+        assert response.command == command
+        assert response.data == data
 
 
 @pytest.mark.parametrize(
@@ -27,7 +86,7 @@ def test_read_reply_packet(mock_lirc, reply_packet):
     [
         (b"BEGIN\nSEND_ONCE remote key\nSUCCESS\nEND\n", 1),
         (b"BEGIN\nSEND_ONCE remote key\nERROR\nEND\n", 1),
-        (b"BEGIN\nSEND_ONCE remote key\nSUCCESS\nEND\n", 1),
+        (b"BEGIN\nSEND_ONCE remote key\nSUCCESS\nEND\n", 5),
     ],
 )
 def test_send_once(mock_lirc, reply_packet, repeat_count):
@@ -37,7 +96,7 @@ def test_send_once(mock_lirc, reply_packet, repeat_count):
     SUCCESS = True if "SUCCESS" in reply_packet.decode(mock_lirc.ENCODING) else False
 
     mock_lirc.socket.recv.return_value = reply_packet
-    response = mock_lirc.send_once(KEY, REMOTE)
+    response = mock_lirc.send_once(KEY, REMOTE, repeat_count=repeat_count)
     mock_lirc.socket.sendall.assert_called_with(
         (COMMAND + "\n").encode(mock_lirc.ENCODING)
     )
