c78e49a3d4bd783f1ea03ef35feddb6bdc4bb8f8
Eugene Triguba <eugenetriguba@gmail.com>
2020-04-28 16:39:59 +0000

    feat: Add defaults for source and sink pipeline in Display init

--- stb/display.py
+++ stb/display.py
@@ -1,15 +1,23 @@
 import gi
+from config_file import ConfigFile
 from gi.repository import Gst
 
 from stb.frame import Frame
+from stb.utils import get_config_file_path
 
 gi.require_version("Gst", "1.0")
 Gst.init(None)
 
 
 class Display:
-    def __init__(self, source_pipeline, sink_pipeline):
-        pass
+    def __init__(self, source_pipeline: str = None, sink_pipeline: str = None):
+        config = ConfigFile(get_config_file_path())
+        self.source_pipeline = (
+            source_pipeline if source_pipeline else config.get("video.source_pipeline")
+        )
+        self.sink_pipeline = (
+            sink_pipeline if sink_pipeline else config.get("video.sink_pipeline")
+        )
 
     def get_frame(self, timeout_secs: float = 10.0) -> Frame:
         pass
